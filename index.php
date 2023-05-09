a44f48da0230ebef2d7cc5ed3e456f240d569463
Mariama Ndao <ndaomariama72@gmail.com>
2023-05-09 21:00:26 +0000

    Affichage des valeurs

--- index.php
+++ index.php
@@ -2,6 +2,14 @@
     $a=rand(1,100);
     $b=rand(1,100);
     $c=rand(1,100);
+    echo "les valeurs generer sont :";
+    echo "<br>";
+    echo "$a";
+    echo "<br>";
+    echo "$b";
+    echo "<br>";
+    echo "$c";
+    echo "<br>";
 ?>
    
    
