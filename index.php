23dea3ffe9d335963737127c973c388f2a487a39
Mariama Ndao <ndaomariama72@gmail.com>
2023-05-09 20:29:49 +0000

    paragphe

--- index.php
+++ index.php
@@ -7,40 +7,6 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-        $a=rand(1,100);
-        $b=rand(1,100);
-        $c=rand(1,100); 
-        echo "les valeurs generer sont :";
-        echo "<br>";
-        echo "$a";
-        echo "<br>";
-        echo "$b";
-        echo "<br>";
-        echo "$c";
-        echo "<br>";
-        if($a>$b && $b>$c){
-            echo "dans l ordre croissant:";
-            echo "<br>";
-            echo $c."<".$b."<".$a;
-            echo "<br>";
-            echo "dans l ordre decroissant:";
-            echo "<br>";
-            echo $a.">".$b.">".$c;
-        
-        }
-        elseif($a>$c&& $c>$b){
-            echo "dans l ordre croissant:";
-            echo "<br>";
-            echo $b."<".$c."<".$a;
-            echo "<br>";
-            echo "dans l ordre decroissant:";
-            echo "<br>";
-            echo $a.">".$c.">".$b;
-        }
-        
-
-    ?>
-    
+    <p>Bonjour</p>
 </body>
 </html>
