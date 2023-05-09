fc36870b8a56b933d865f18b20c7a4a818fae2d4
Mariama Ndao <ndaomariama72@gmail.com>
2023-05-09 21:06:07 +0000

    Affiher les trois elements generer dans l ordre croissant et descroissant

--- index.php
+++ index.php
@@ -10,6 +10,68 @@
     echo "<br>";
     echo "$c";
     echo "<br>";
+    if($a>$b && $b>$c){
+        echo "dans l ordre croissant:";
+        echo "<br>";
+        echo $c."<".$b."<".$a;
+        echo "<br>";
+        echo "dans l ordre decroissant:";
+        echo "<br>";
+        echo $a.">".$b.">".$c;
+    
+    }
+    elseif($a>$c&& $c>$b){
+        echo "dans l ordre croissant:";
+        echo "<br>";
+        echo $b."<".$c."<".$a;
+        echo "<br>";
+        echo "dans l ordre decroissant:";
+        echo "<br>";
+        echo $a.">".$c.">".$b;
+    
+    }
+    elseif($b>$a && $a>$c){
+        echo "dans l ordre croissant:";
+        echo "<br>";
+        echo $c."<".$a."<".$b;
+        echo "<br>";
+        echo "dans l ordre decroissant:";
+        echo "<br>";
+        echo $b.">".$a.">".$c;
+    
+    }
+    elseif($b>$c && $c>$a){
+        echo "dans l ordre croissant:";
+        echo "<br>";
+        echo $a."<".$c."<".$b;
+        echo "<br>";
+        echo "dans l ordre decroissant:";
+        echo "<br>";
+        echo $b.">".$c.">".$a;
+        echo "<br>";
+    
+    }
+    elseif($c>$a && $a>$b){
+        echo "dans l ordre croissant:";
+        echo "<br>";
+        echo $b."<".$a."<".$c;
+        echo "<br>";
+        echo "dans l ordre decroissant:";
+        echo "<br>";
+        echo $c.">".$a.">".$b;
+    
+    }
+    elseif($c>$b && $b>$a){
+        echo "dans l ordre croissant:";
+        echo "<br>";
+        echo $a."<".$b."<".$c;
+        echo "<br>";
+        echo "dans l ordre decroissant:";
+        echo "<br>";
+        echo $c.">".$b.">".$a;
+    
+    }
+    
 ?>
    
    
